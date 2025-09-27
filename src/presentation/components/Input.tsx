cc5d543b13218cb0385d5074257128e58632d6a0
Maju Facirolli <majufacirolli2004@hotmail.com>
2025-09-27 12:18:09 -0300

    refactor: adds label to input component

--- src/presentation/components/Input.tsx
+++ src/presentation/components/Input.tsx
@@ -1,11 +1,19 @@
-import type { InputHTMLAttributes, FC } from 'react';
+import type { FC, InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   className?: string;
+  label?: string;
 }
 
-const Input: FC<InputProps> = ({ className = '', ...props }) => (
-  <input className={`input input-bordered w-full ${className}`} {...props} />
+const Input: FC<InputProps> = ({ className = "", label, ...props }) => (
+  <div className="w-full flex flex-col gap-1 items-start">
+    {label && (
+      <label htmlFor="" className="fieldset-label text-sm">
+        {label}
+      </label>
+    )}
+    <input className={`input input-bordered w-full ${className}`} {...props} />
+  </div>
 );
 
 export default Input;
