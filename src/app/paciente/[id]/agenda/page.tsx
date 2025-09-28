bfa478127796fe25716642402749de6e46fd583e
Maju Facirolli <majufacirolli2004@hotmail.com>
2025-09-28 02:08:35 -0300

    refactor: updates agenda page presentation

--- src/app/paciente/[id]/agenda/page.tsx
+++ src/app/paciente/[id]/agenda/page.tsx
@@ -1,4 +1,6 @@
+import { Calendar } from "lucide-react";
 import { ScheduleAppointmentChat } from "@/presentation/components/ScheduleAppointmentChat";
+import { PatientAgenda } from "@/presentation/components/partials/PatientAgenda";
 
 export default function DoctorAgendaPage({
   params,
@@ -6,15 +8,21 @@ export default function DoctorAgendaPage({
   params: { id: string };
 }) {
   return (
-    <main className="p-8">
-      <h1 className="text-2xl font-bold mb-4">Agenda do Médico</h1>
-      <p>Médico ID: {params.id}</p>
-      {/* Aqui você pode adicionar a tabela de horários, consultas, etc. */}
-      <div className="mt-6">
-        <h2 className="text-xl font-semibold mb-2">Horários</h2>
-        <div className="border rounded p-4 bg-gray-50">
-          <p>Nenhum horário cadastrado.</p>
-        </div>
+    <main className="space-y-6 mt-8">
+      <div className="space-y-1">
+        <h1 className="text-2xl font-bold flex items-center gap-1">
+          <Calendar className="size-8 shrink-0" />
+          Agenda
+        </h1>
+        <p>
+          Aqui você pode consultar seus próximos agendamentos e agendar uma
+          consulta com o <strong>Assistente de Agendamento!</strong>
+        </p>
+      </div>
+
+      <div>
+        <h2 className="text-xl font-semibold mb-2">Próximos agendamentos</h2>
+        <PatientAgenda patientId={params?.id} />
       </div>
 
       <ScheduleAppointmentChat />
