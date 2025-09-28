17db3e718f8edd19ed6efdb007538c53492ec0df
Maju Facirolli <majufacirolli2004@hotmail.com>
2025-09-28 02:22:23 -0300

    refactor: udpates patient history page

--- src/app/paciente/[id]/historico/page.tsx
+++ src/app/paciente/[id]/historico/page.tsx
@@ -1,21 +1,22 @@
-import React from "react";
+import { History } from "lucide-react";
+import { PatientHistory } from "@/presentation/components/partials/PatientHistory";
 
-export default function DoctorAgendaPage({
+export default function PatientHistoryPage({
   params,
 }: {
   params: { id: string };
 }) {
   return (
-    <main className="p-8">
-      <h1 className="text-2xl font-bold mb-4">Agenda do Médico</h1>
-      <p>Médico ID: {params.id}</p>
-      {/* Aqui você pode adicionar a tabela de horários, consultas, etc. */}
-      <div className="mt-6">
-        <h2 className="text-xl font-semibold mb-2">Horários</h2>
-        <div className="border rounded p-4 bg-gray-50">
-          <p>Nenhum horário cadastrado.</p>
-        </div>
+    <section className="space-y-6 mt-8">
+      <div className="space-y-1">
+        <h1 className="text-2xl font-bold flex items-start gap-1">
+          <History className="size-8 shrink-0" />
+          Histórico de Agendamentos
+        </h1>
+        <p>Aqui você pode consultar todos os seus agendamentos.</p>
       </div>
-    </main>
+
+      <PatientHistory patientId={params?.id} />
+    </section>
   );
 }
